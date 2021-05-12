addc2e62cc7e6267e3a5c77ab6d49c616fbf5142
Helton Ricardo <50843386+heltonr13@users.noreply.github.com>
2021-05-11 22:49:58 -0300

    aplicando efeito de escopo

--- src/components/shared/painel/Painel.vue
+++ src/components/shared/painel/Painel.vue
@@ -12,7 +12,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .painel {
   padding: 0 auto;
   border: solid 2px gray;
@@ -32,5 +32,9 @@ export default {
   margin: 0 0 15px 0;
   padding: 10px;
   text-transform: uppercase;
+}
+
+* {
+  box-shadow: 5px 5px 5px;
 }
 </style>
