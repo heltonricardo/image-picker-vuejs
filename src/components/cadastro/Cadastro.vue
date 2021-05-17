64de178a03fc4e39e61ffae6565e2d15eb9948ea
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-05-17 09:26:22 -0300

    Update Cadastro.vue

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -1,11 +1,69 @@
+<!-- alurapic/src/components/cadastro/Cadastro.vue -->
+
 <template>
-  <h2 class="centralizado">Cadastro</h2>
+  <div>
+    <h1 class="centralizado">Cadastro</h1>
+    <h2 class="centralizado"></h2>
+
+    <form>
+      <div class="controle">
+        <label for="titulo">TÍTULO</label>
+        <input id="titulo" autocomplete="off" />
+      </div>
+
+      <div class="controle">
+        <label for="url">URL</label>
+        <input id="url" autocomplete="off" />
+        <imagem-responsiva />
+      </div>
+
+      <div class="controle">
+        <label for="descricao">DESCRIÇÃO</label>
+        <textarea id="descricao" autocomplete="off"></textarea>
+      </div>
+
+      <div class="centralizado">
+        <meu-botao rotulo="GRAVAR" tipo="submit" />
+        <router-link to="/"
+          ><meu-botao rotulo="VOLTAR" tipo="button"
+        /></router-link>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
+import ImagemResponsiva from "../shared/imagem-responsiva/ImagemResponsiva.vue";
+import Botao from "../shared/botao/Botao.vue";
+
+export default {
+  components: {
+    "imagem-responsiva": ImagemResponsiva,
+    "meu-botao": Botao,
+  },
+};
 </script>
 
 <style scoped>
+.centralizado {
+  text-align: center;
+}
+.controle {
+  font-size: 1.2em;
+  margin-bottom: 20px;
+}
+.controle label {
+  display: block;
+  font-weight: bold;
+}
+
+.controle label + input,
+.controle textarea {
+  width: 100%;
+  font-size: inherit;
+  border-radius: 5px;
+}
+
 .centralizado {
   text-align: center;
 }
