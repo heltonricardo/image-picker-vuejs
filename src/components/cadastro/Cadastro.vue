66c9b2281ad1577014c2a50a0c1931cea522417a
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-05-17 11:59:42 -0300

    Update Cadastro.vue

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -63,8 +63,10 @@ export default {
 
   methods: {
     grava() {
-      console.log(this.foto.titulo, this.foto.url, this.foto.descricao);
-      this.foto = new Foto();
+      this.$http.post("http://localhost:3000/v1/fotos", this.foto).then(
+        () => (this.foto = new Foto()),
+        (err) => console.log(err)
+      );
     },
   },
 };
