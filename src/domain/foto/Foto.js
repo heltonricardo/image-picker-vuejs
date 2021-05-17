dae89b79bcc6ee1eaca6fdf11a590503d51497f5
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-05-17 13:42:22 -0300

    Update Foto.js

--- src/domain/foto/Foto.js
+++ src/domain/foto/Foto.js
@@ -1,5 +1,5 @@
 export default class Foto {
-  constructor(titulo="", url="", descricao="") {
+  constructor(titulo = "", url = "", descricao = "") {
     this.titulo = titulo;
     this.url = url;
     this.descricao = descricao;
