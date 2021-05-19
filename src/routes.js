813943f29bcbb09bbfdbeab56e385407b081a6b8
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-05-19 11:07:32 -0300

    configuracao para build do projeto

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,15 @@
-import Cadastro from "./components/cadastro/Cadastro";
+/* O Cadastro não foi importado como Home para que possamos fazer o que é
+ * chamado de lazy loading. Supondo que o usuário acesse com muito mais
+ * frequência a Home, criando a função que retorna o Cadastro fará com que esses
+ * arquivos só sejam carregados quando a página for acessada, e não quando a
+ * aplicação é carregada, como acontece com a Home. O procedimento citado serve
+ * para quando ocorre build do projeto através do comando "npm run build". Após
+ * executar esse comando, copie o index.html e os arquivos build.js da pasta
+ * dist para o servidor escolhido.
+ */
 import Home from "./components/home/Home";
+const Cadastro = () =>
+  System.import("./components/cadastro/Cadastro").then(m => m.default);
 
 export const routes = [
   { path: "/", name: "home", component: Home, titulo: "Home", menu: true },
