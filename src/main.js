8fd92e0fd852cd5d43ef0978cbe0de2298bacf4b
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-05-19 10:08:52 -0300

    definindo mensagens de erro para pt-br

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import VueRouter from "vue-router";
 import { routes } from "./routes";
 import "./directives/Transform";
 import VeeValidate from "vee-validate";
+import msg from "./pt_BR";
 
 Vue.use(VueResource);
 Vue.http.options.root = "http://localhost:3000";
@@ -16,7 +17,14 @@ Vue.use(VueRouter);
  */
 const router = new VueRouter({ routes, mode: "history" });
 
-Vue.use(VeeValidate);
+Vue.use(VeeValidate, {
+  locale: "pt_BR",
+  dictionary: {
+    pt_BR: {
+      messages: msg
+    }
+  }
+});
 
 // A mesma técnica do exemplo acima:
 new Vue({
