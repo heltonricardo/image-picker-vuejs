326f40f2f03bb1beddc63f191e203d97c97f77fa
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-05-20 09:11:00 -0300

    carregando imagens localmente

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- <img src="/static/teste.png"/> Usando imagem local -->
     <h1 class="centralizado">{{ titulo }}</h1>
     <p v-show="mensagem" class="centralizado">{{ mensagem }}</p>
 
